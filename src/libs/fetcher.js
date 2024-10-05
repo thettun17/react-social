7d729a30a9966d79583ae412e10e50a2ad9c81bb
Thet Tun <thettun.loard17@gmail.com>
2024-10-05 15:50:22 +0700

    add like and comment apis

--- src/libs/fetcher.js
+++ src/libs/fetcher.js
@@ -1,114 +1,171 @@
 const api = import.meta.env.VITE_API;
 
 function getToken() {
-    return localStorage.getItem("token")
+  return localStorage.getItem("token");
 }
 
 export async function postUser(data) {
-    const res = await fetch(`${api}/users`, {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        }
-    })   
+  const res = await fetch(`${api}/users`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-    if(res.ok) {
-        return res.json();
-    }
-    
-    throw new Error("Error: Check Network Log");
-    
+  if (res.ok) {
+    return res.json();
+  }
+
+  throw new Error("Error: Check Network Log");
 }
 
 export async function postLogin(username, password) {
-    const res = await fetch(`${api}/login`, {
-        method: "POST",
-        body: JSON.stringify({username, password}),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
+  const res = await fetch(`${api}/login`, {
+    method: "POST",
+    body: JSON.stringify({ username, password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if(res.ok) {
-        return res.json();
-    }
+  if (res.ok) {
+    return res.json();
+  }
 
-    throw new Error("Incorrect username or password")
+  throw new Error("Incorrect username or password");
 }
 
 export async function fetchUser(id) {
-    const token = getToken();
-    const res = await fetch(`${api}/users/${id}`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    })
-    return res.json();
+  const token = getToken();
+  const res = await fetch(`${api}/users/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return res.json();
 }
 
 export async function fetchVerify() {
-    const token = getToken();
-    const res = await fetch(`${api}/verify`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    })
+  const token = getToken();
+  const res = await fetch(`${api}/verify`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-    if(res.ok) {
-        return res.json();
-    }
+  if (res.ok) {
+    return res.json();
+  }
 
-    return false;
+  return false;
 }
 
 export async function postPost(content) {
-    const token = getToken();
-    const res = await fetch(`${api}/content/posts`, {
-        method: "POST",
-        body: JSON.stringify({content}),
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        }
-    })
-    if(res.ok) {
-        return res.json()
-    }
-    throw new Error("Error: Check Network Log")
+  const token = getToken();
+  const res = await fetch(`${api}/content/posts`, {
+    method: "POST",
+    body: JSON.stringify({ content }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (res.ok) {
+    return res.json();
+  }
+  throw new Error("Error: Check Network Log");
 }
 
 export async function postComment(content, postId) {
-    const token = getToken();
-    const res = await fetch(`${api}/content/comments`, {
-        method: "POST",
-        body: JSON.stringify({content, postId}),
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        }
-    })
-    if(res.ok) {
-        return res.json()
-    }
-    throw new Error("Error: Check Network Log")
+  const token = getToken();
+  const res = await fetch(`${api}/content/comments`, {
+    method: "POST",
+    body: JSON.stringify({ content, postId }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (res.ok) {
+    return res.json();
+  }
+  throw new Error("Error: Check Network Log");
 }
 
-
 export async function deletePost(id) {
-    const token = getToken();
-    const res = await fetch(`${api}/content/posts/${id}`, {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        }
-    })
+  const token = getToken();
+  const res = await fetch(`${api}/content/posts/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-    if(res.ok) {
-        return res
-    }
+  if (res.ok) {
+    return res;
+  }
 
-    throw new Error("Error: Check Network Log")
+  throw new Error("Error: Check Network Log");
+}
+
+export async function postPostLike(id) {
+  const token = getToken();
+  const res = await fetch(`${api}/content/like/posts/${id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return res.json();
+}
+
+export async function postCommentLike(id) {
+  const token = getToken();
+  const res = await fetch(`${api}/content/like/comments/${id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return res.json();
+}
+
+export async function deletePostLike(id) {
+  const token = getToken();
+  const res = await fetch(`${api}/content/unlike/posts/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return res.json();
+}
+
+export async function deleteCommentLike(id) {
+  const token = getToken();
+  const res = await fetch(`${api}/content/unlike/comments/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return res.json();
+}
+
+export async function fetchPostLikes(id) {
+  const res = await fetch(`${api}/content/like/posts/${id}`);
+  return res.json();
+}
+
+export async function fetchCommentLikes(id) {
+  const res = await fetch(`${api}/content/like/comments/${id}`);
+  return res.json();
 }
